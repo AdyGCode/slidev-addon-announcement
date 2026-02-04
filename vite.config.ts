7479bf0df3e3020a1c1a7e2b4b756fe8ce60e4ab
Adrian Gould <adrian.gould@nmtafe.wa.edu.au>
2026-02-04 16:16:45 +0800

    fix: minor general fixes, bump version

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [
-    Slidev({
-      // ...
-      addons: [
-        'slidev-addon-announcement'
-      ]
-    })
-  ],
-})
+  optimizeDeps: {
+    include: [
+      'slidev-addon-announcement'
+    ]
+  },
+});
